3b312b3a1ddefc961445059ad225029bea7aaa81
Suyash Pawar <suyashani99@gmail.com>
2026-05-05 00:08:38 +0530

    fix render porting

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ app.use(errorHandler)
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`.cyan)
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`)
 })
